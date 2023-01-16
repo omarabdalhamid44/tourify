324f3dc66be59724a995bfc2c420e5bfcbb14a2f
omarabdalhamid44 <omarabdalhmaid44@gamil.com>
2023-01-16 13:46:12 +0200

    Tourify app

---
+++ lib/utils/VeriableConst/listOf_Thing.dart
@@ -0,0 +1,29 @@
+
+class ListOfThings {
+  static const list_of_countries = <String>[
+    '🇵🇸 Palestine',
+    '🇯🇴 Jordan',
+    '🇺🇸 USA',
+    '🇪🇬 Egept',
+    '🇸🇦 SoudiAribia'
+  ];
+  static const list_of_price = <String>[
+    '00 \$',
+    '05 \$',
+    '10 \$',
+    '20 \$',
+    '30 \$',
+    '40 \$',
+    '50 \$',
+  ];
+  static const list_of_number = <String>[
+    '🇵🇸 00972',
+    '🇺🇸 001',
+    '🇯🇴 0096',
+    '🇪🇬 0055',
+    '🇸🇦 54'
+  ];
+  static const list_of_gender = <String>['Male', 'FeMale'];
+  static const list_of_Driver = <String>['Kia', 'Toyta'];
+
+}
